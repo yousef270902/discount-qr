c224a5ca12453de6cf2fea016734ae872d725551
youssef Wahba <150840439+yousef270902@users.noreply.github.com>
2026-01-13 14:47:46 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,12 @@
+window.APP_DATA = {
+  settings: {
+    discount: 25,               // نسبة الخصم
+    expire: "2026-01-31"        // تاريخ الانتهاء
+  },
+
+  products: {
+    1: { name: "منتج 1", price: 100 },
+    2: { name: "منتج 2", price: 200 },
+    3: { name: "منتج 3", price: 300 }
+  }
+};
